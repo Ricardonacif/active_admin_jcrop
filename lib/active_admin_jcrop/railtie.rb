b36fbeaaa1f8ca0e11d94509de33cb2a6885968b
Ricardo Nacif <ricardonacif@lancedavez.com.br>
2014-06-28 02:20:56 -0300

    fix railtie module

--- lib/active_admin_jcrop/railtie.rb
+++ lib/active_admin_jcrop/railtie.rb
@@ -1,20 +1,23 @@
-class ActiveAdminJcropRailtie < Rails::Railtie
-  initializer "active_admin_jcrop_railtie.configure_rails_initialization" do
-    ActiveAdmin::DSL.send(:include, ActiveAdminJcrop::DSL)
-    ActiveAdmin.setup do |config|
-      [ 'active_admin_jcrop/crop_modal',
-        "active_admin_jcrop/jquery.color",
-        "active_admin_jcrop/jquery.Jcrop.min"
-      ].each do |js|
-        config.register_javascript js
-      end
+module ActiveAdminJcrop
+  class Railtie < Rails::Railtie
+    initializer "active_admin_jcrop_railtie.configure_rails_initialization" do
+      ActiveAdmin::DSL.send(:include, ActiveAdminJcrop::DSL)
+      ActiveAdmin.setup do |config|
+        [ 'active_admin_jcrop/crop_modal',
+          "active_admin_jcrop/jquery.color",
+          "active_admin_jcrop/jquery.Jcrop.min"
+        ].each do |js|
+          config.register_javascript js
+        end
 
-      [ "active_admin_jcrop/crop_modal",
-        'active_admin_jcrop/jquery.Jcrop.min'
-      ].each do |css|
-        config.register_stylesheet css
+        [ "active_admin_jcrop/crop_modal",
+          'active_admin_jcrop/jquery.Jcrop.min'
+        ].each do |css|
+          config.register_stylesheet css
+        end
+        
       end
-      
     end
   end
+
 end
